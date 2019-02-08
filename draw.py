5b709b44a4ce0f14f54f1a3ccbfef560930d8f86
caw024 <caw@stuy.edu>
2019-02-08 11:44:08 -0500

    improve it

--- draw.py
+++ draw.py
@@ -1,4 +1,52 @@
 from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
-    pass
+        A = y1 - y0
+        B = x0 - x1
+        m = -A/B
+
+        x = x0
+        y = y0
+
+        #octant 1
+        if (1 > m) & (m >= 0):
+                d = 2*A + B
+                while x <= x1:
+                        plot(x,y)
+                        if d>0:
+                                y++
+                                d+= 2*B
+                x++
+                d+=2*A
+                                #octant 2
+        else if m >= 1:
+            d = 2*B + A
+            while y<= y1:
+                plot(x,y)
+                if d<0:
+                    x++
+                    d+= 2*A
+                    y++
+                    d+=2*B
+            
+                    #octant 3 - not done
+        else if (-1 < m) & (m < 0):
+        d = 2*A + B
+        while x <= x1:
+            plot(x,y)
+            if d>0:
+            y++
+                d+= 2*B
+                x++
+            d+=2*A
+        
+    #octant 4 - not done
+    else if (-1 <= m):
+    d = 2*A + B
+    while x <= x1:
+            plot(x,y)
+            if d>0:
+                y++
+                d+= 2*B
+                x++
+                d+=2*A
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from draw import *
 
 screen = new_screen()
 color = [ 0, 255, 0 ]
+draw_line(0,0,50,60,screen,color)
+
 
 display(screen)
 save_extension(screen, 'img.png')
