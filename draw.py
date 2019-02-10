1990345a67aa2e9392ac86460b724d83e14a3ac7
Calvin Aw <caw@stuy.edu>
2019-02-10 18:23:09 -0500

    cool

--- draw.py
+++ draw.py
@@ -1,52 +1,54 @@
 from display import *
+#eq of line is Ax+By+C=0
 
-def draw_line( x0, y0, x1, y1, screen, color ):
-        A = y1 - y0
-        B = x0 - x1
-        m = -A/B
-
-        x = x0
-        y = y0
-
-        #octant 1
-        if (1 > m) & (m >= 0):
-                d = 2*A + B
-                while x <= x1:
-                        plot(x,y)
-                        if d>0:
-                                y++
-                                d+= 2*B
-                x++
-                d+=2*A
-                                #octant 2
-        else if m >= 1:
-            d = 2*B + A
-            while y<= y1:
-                plot(x,y)
-                if d<0:
-                    x++
-                    d+= 2*A
-                    y++
-                    d+=2*B
-            
-                    #octant 3 - not done
-        else if (-1 < m) & (m < 0):
-        d = 2*A + B
-        while x <= x1:
-            plot(x,y)
+def draw_line( x0, y0, x1, y1, screen, color):
+   A = y1 - y0 #change in y
+   B = x0 - x1 #negative change in x
+   x = x0
+   y = y0
+   if B==0:
+      while y <= y1:
+         plot(screen,color,x,y)
+         y+=1
+   else:
+      m = -1.0*A/B #slope  #octant 1
+      if ((1 > m) & (m >= 0)):
+         d = 2*A + B
+         while x <= x1:
+            plot(screen,color,x,y)
+            if d>0: #above line
+               y+=1
+               d+= 2*B
+            x+=1
+            d+=2*A             #octant 2
+      elif m >= 1:
+         d = 2*B + A
+         while y<= y1:
+            plot(screen, color,x,y)
+            if d<0:
+               x+=1
+               d+=2*A
+            y+=1
+            d+=2*B
+            #octant 8 - not done     
+      elif ((-1 < m) & (m < 0)):
+         d = 2*A-B
+         while x <= x1:
+            plot(screen,color,x,y)
+            if d>=0:
+               y-=1
+               d-=2*B
+            x+=1
+            d+=2*A  #octant 7 - not done
+         
+      else:
+         d = A - 2*B
+         while y >= y1:
+            plot(screen, color,x,y)
             if d>0:
-            y++
-                d+= 2*B
-                x++
-            d+=2*A
-        
-    #octant 4 - not done
-    else if (-1 <= m):
-    d = 2*A + B
-    while x <= x1:
-            plot(x,y)
-            if d>0:
-                y++
-                d+= 2*B
-                x++
-                d+=2*A
+               x+=1
+               d+=2*A
+            y-=1
+            d-=2*B
+
+pass
--- main.py
+++ main.py
@@ -2,8 +2,37 @@ from display import *
 from draw import *
 
 screen = new_screen()
+
+'''
+#O1
 color = [ 0, 255, 0 ]
-draw_line(0,0,50,60,screen,color)
+draw_line(0,0,200,100,screen,color)
+
+
+#O2
+color = [ 255, 0, 0 ]
+draw_line(50,0,150,200,screen,color)
+
+
+#O3
+color = [ 0, 255, 0 ]
+draw_line(0,95,60,76,screen,color)
+
+
+#O4
+color = [ 0, 255, 255 ]
+draw_line(45,120,60,62,screen,color)
+'''
+
+
+
+n = 300
+while (n >= 0):
+    color = [n % 255, (-1 * n) % 255, 255]
+    draw_line(0,n,300-n,0,screen,color)
+    n-=5
+
+
 
 
 display(screen)
